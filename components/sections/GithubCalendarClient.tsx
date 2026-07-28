82e553225e3023226c9b4c4b6a4dc7da911e4823
xAbdull7 <m3llemgamer1999@gmail.com>
2026-07-29 00:59:19 +0300

    feat: add github contributions calendar

---
+++ components/sections/GithubCalendarClient.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+import { useTheme } from 'next-themes';
+
+export default function GithubCalendarClient({ username }: { username: string }) {
+  const { resolvedTheme } = useTheme();
+  
+  return (
+    <div className="w-full overflow-hidden flex justify-center mt-2">
+      <GitHubCalendar 
+        username={username} 
+        colorScheme={resolvedTheme === 'dark' ? 'dark' : 'light'}
+        hideTotalCount
+        hideColorLegend
+        theme={{
+          light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
+          dark: ['#161b22', '#0e4429', '#006d32', '#26a641', '#39d353'],
+        }}
+        labels={{
+          totalCount: '{{count}} contributions in the last year',
+        }}
+      />
+    </div>
+  );
+}
